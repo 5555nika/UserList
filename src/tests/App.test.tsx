1aa273ade33dda228363def50df00ca689f86513
5555nika <niiarasisman@gmail.com>
2026-08-12 16:46:57 +0300

    Initial

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,5 +1,6 @@
+/// <reference types="vitest/globals" />
 import { render, screen, fireEvent } from '@testing-library/react';
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import '@testing-library/jest-dom';
 import App from '../App';
 describe('App component', () => {
